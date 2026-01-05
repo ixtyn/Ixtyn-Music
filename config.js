46f94bd721530d5b40e08453ebc2bb6f323900ab
Ｉ Ｘ Ｔ Ｙ Ｎ _ ✗ <113636833+ixtyn@users.noreply.github.com>
2026-01-05 10:46:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
+  ownerID: ["518640079556837379", ""], 
+  mongodbUri : "mongodb+srv://bbbcc717_db_user:Zz7oo8xCauqXr1M6@cluster0.pqycphs.mongodb.net/?appName=Cluster0",
   spotifyClientId : "d92baed9605a45a39ed7c2a2d960b1c1",
   spotifyClientSecret : "e9b29f6739de4315bc03b6d8a8e93b03",
   setupFilePath: './commands/setup.json',
